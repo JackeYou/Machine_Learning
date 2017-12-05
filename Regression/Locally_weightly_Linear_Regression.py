c06d6e25de0b291272e41e2476b62d08b586ca35
1sco <282879021@qq.com>
2017-12-05 22:11:52 +0800

    修改

--- Regression/Locally_weightly_Linear_Regression.py
+++ Regression/Locally_weightly_Linear_Regression.py
@@ -2,45 +2,26 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 from math import exp
-
 from mpmath.matrices import linalg
 from numpy import linalg
 from numpy.linalg import linalg
 from numpy.ma.core import shape
 from numpy.matlib import eye
-
 import matplotlib.pyplot as plt
 
-#import demo
-
-
-def lwlr(testPoint, xArr, yArr, k = 1.0):
-	xMat = np.mat(xArr)
-	yMat = np.mat(yArr).T
-	#zMat = xMat * yMat
-
-	m = shape(xMat)[0]
-	weights = np.mat(eye(m))
-	for i in range(m):
-		diffMat = np.mat(testPoint) - xMat[i, :]
-		print xMat[i, :]
-		weights[i, i] = exp(diffMat * diffMat.T / (-2 * k**2))
-	xTx = np.mat(xMat.T * (weights * xMat))
-	if linalg.det(xTx) == 0:
-		print "This Matrix is singular, cannot do inverse"
-		return
-	theta = xTx.I * (xMat.T * (weights * yMat))
-	#print (testPoint * theta)[:,0]
-	#print type(testPoint * theta)
-	return (testPoint * theta)
-
-def lwlrText(textArr, xArr, yArr, k = 1.0):
-	m = shape(textArr)[0]
-	yHat = np.zeros(m)
-	for i in range(m):
-		yHat[i] = lwlr(textArr[i], xArr, yArr, k)
-	return yHat
-
+#加载数据
+def loadDataSet(filename):
+	xList = []
+	yList = []
+	with open(filename) as fn:
+		for i in fn:
+			x = i.rstrip().split("\t")
+			#x = map(eval, x)  此函数eval容易造成恶意输入
+			x = map(eval, x)
+			xList.append(x[: -1])
+			yList.append(float(x[-1]))
+	return xList, yList
+'''
 def loadDataSet(filename):
 	numFeature = len(open(filename).readline().split("\t")) - 1
 	dataMat = []
@@ -58,14 +39,36 @@ def loadDataSet(filename):
 		labelMat.append(float(curArr[-1]))
 		#print labelMat
 	return dataMat, labelMat
+'''
 
-xArr, yArr = loadDataSet("/home/liud/PycharmProjects/Machine_Learning/Regression/data/ex0.txt")
-#xArr, yArr = demo.loadDataSet("/home/liud/桌面/ex0.txt")
-print xArr[0]
-print "k = 1.0 : ", lwlr(xArr[0], xArr, yArr, k = 1.0)
-print "k = 0.1 : ", lwlr(xArr[0], xArr, yArr, k = 0.1)
-print "k = 0.01 : ", lwlr(xArr[0], xArr, yArr, k = 0.01)
+def lwLR(testPointList, xList, yList, k = 1.0):
+	xArr = np.array(xList)
+	yArr = np.transpose([yList])
+	testPointArr = np.array(testPointList)
+	arrDot = lambda x, y: np.dot(x, y)
+	m = xArr.shape[0]
+	weights = np.eye(m)
+	for i in range(m):
+		diffArr = np.array(testPointList) - xArr[i]
+		weights[i, i] = exp(np.dot(diffArr, np.transpose([diffArr])) / (-2 * k**2))
+	xTx = np.dot(xArr.T, np.dot(weights, xArr))
+	if linalg.det(xTx) == 0:
+		print "This Matrix is singular, cannot do inverse"
+		return
+	theta = arrDot(np.linalg.inv(xTx),
+				   arrDot(xArr.T,
+						  arrDot(weights, yArr)))
+	#print arrDot(testPointArr, theta)[0]
+	return arrDot(testPointArr, theta)[0]
 
+def lwlrText(textArr, xArr, yArr, k = 1.0):
+	m = shape(textArr)[0]
+	yHat = np.zeros(m)
+	for i in range(m):
+		yHat[i] = lwLR(textArr[i], xArr, yArr, k)
+	return yHat
+
+#图像展示
 def showlwlr():
 	yHat = lwlrText(xArr, xArr, yArr, 0.03)
 	xMat = np.mat(xArr)
@@ -79,4 +82,16 @@ def showlwlr():
 	ax.scatter(xMat[:, 1].flatten().A[0],np.mat(yArr).T[:, 0].flatten().A[0],s = 2,c = 'red')
 	plt.show()
 
-showlwlr()
+#主函数
+def main():
+	xList, yList = loadDataSet("/home/liud/PycharmProjects/Machine_Learning/Regression/data/ex0.txt")
+	# xArr, yArr = demo.loadDataSet("/home/liud/桌面/ex0.txt")
+	print xList[0]
+	print "k = 1.0 : ", lwLR(xList[0], xList, yList, k = 1.0)
+	print "k = 0.1 : ", lwLR(xList[0], xList, yList, k = 0.1)
+	print "k = 0.01 : ", lwLR(xList[0], xList, yList, k = 0.01)
+	showlwlr()
+
+if __name__ == '__main__':
+	main()
+	print 'Success'
