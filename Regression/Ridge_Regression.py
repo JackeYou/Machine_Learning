c078c5ecd15e09e1405d809d5eb17b4b856d241e
1sco <282879021@qq.com>
2017-12-06 17:26:55 +0800

    岭回归

--- Regression/Ridge_Regression.py
+++ Regression/Ridge_Regression.py
@@ -3,7 +3,13 @@
 
 '''
 	岭回归：
-
+	有些情况下无法按照上面的典型回归的方法去训练模型.
+	比如，训练样本数量少,甚至少于样本维数,这样将导致数据矩阵无法求逆；
+	又比如样本特征中存在大量相似的特征,导致很多参数所代表的意义重复.
+	总得来说，就是光靠训练样本进行无偏估计是不好用了.
+	这个时候，我们就应用结构风险最小化的模型选择策略，在经验风险最小化的基础上加入正则化因子.
+	当正则化因子选择为模型参数的二范数的时候,整个回归的方法就叫做岭回归.
+	w = 1/(xTx + λI) * xTy
 '''
 import matplotlib.pyplot as plt
 import numpy as np
@@ -52,7 +58,6 @@ def regularize(xArr, yArr):
 #岭回归公式：w = (xTx + λI).I * xTy
 def ridgeRegression(xArr, yArr, ld=0.2):
 	xTx = np.dot(xArr.T, xArr)
-	#print ld
 	penalty_term = xTx + np.eye(np.shape(xTx)[1]) * ld # 这里面的*运算符对应的是np.multiply()方法，是实现对应元素相乘.
 	#print penalty_term
 	if np.linalg.det(penalty_term) == 0.0:
@@ -71,11 +76,11 @@ def ridgeTest(xList, yList, numTestPts = 30):
 	for i in xrange(numTestPts):
 		ws = ridgeRegression(xArr, yArr, exp(i - 10))
 		wArr[i, :] = ws.T
-		print wArr[i]
+		#print wArr[i]
 	return wArr
 
 #展示结果
-def showRidge():
+def showRidge(ridgeWeights):
 	fig = plt.figure()
 	ax = fig.add_subplot(111)
 	ax.plot(ridgeWeights)
@@ -85,7 +90,7 @@ def main():
 	xList, yList = loadDataSet("/home/liud/PycharmProjects/Machine_Learning/Regression/data/abalone.txt")
 	ridgeWeights = ridgeTest(xList, yList)
 	print ridgeWeights
-	showRidge()
+	showRidge(ridgeWeights)
 
 if __name__ == '__main__':
 	main()
