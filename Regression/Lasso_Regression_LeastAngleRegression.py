559c9f7e3bcc97d5e9012dd92a736e33c784425b
1sco <282879021@qq.com>
2017-12-15 10:35:55 +0800

    Lasso算法的最小角回归解法

--- Regression/Lasso_Regression_LeastAngleRegression.py
+++ Regression/Lasso_Regression_LeastAngleRegression.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 '''
 	Least_Angle_Regression最小角回归法
+
 '''
 #加载的包
-import itertools
 import numpy as np
 from math import exp
 from sklearn import linear_model
@@ -40,20 +40,26 @@ def corCoef(xVector, yVector):
 		   / ((np.var(xVector) * np.var(yVector)) ** 0.5)
 
 #Least_Angle_Regression最小角回归法
-def lassoLeastAngleRegression(xArr, yArr, numIt=100):
+def lassoLeastAngleRegression(xArr, yArr, lm=0.2, numIt=350):
 	m, n = np.shape(xArr)
 	w = np.zeros((n, 1)) #初始化系数
-	wTotal = np.zeros([numIt, n])
+	wTotal = []
 	wMax = np.copy(w)
-	Rss = lambda x, y, w: np.dot((y - np.dot(x, w)).T, (y - np.dot(x, w)))
+	#Rss = lambda x, y, w: np.dot((y - np.dot(x, w)).T, (y - np.dot(x, w)))
 	for i in xrange(numIt):
-		lowestError = np.inf
-		for j in xrange(n):
-			pass
+		lowestError = yArr - np.dot(xArr, w)
+		corr = lowestError.T.dot(xArr)[0]
+		IS = np.abs(corr).argmax()
+		corrStar = corr[IS]
+		w[IS] += lm * corrStar / abs(corrStar)
+		wTotal.append(w[:, -1].tolist())
+		print w[:, -1]
+	plt.plot(wTotal)
+	plt.show()
 
-#调sklearn的包进行Least_Angle_Regression最小角回归法
-def sklearn_LassoLeastAngleRegression(xList, yList, lm = 0.2, threshold = 0.1):
-	reg = linear_model.LassoLars(alpha = .1, fit_intercept = False)
+#调skLearn的包进行Least_Angle_Regression最小角回归法
+def skLearn_LassoLeastAngleRegression(xList, yList, lm = 0.2, threshold = 0.1):
+	reg = linear_model.LassoLars(alpha = lm, fit_intercept = False)
 	reg.fit(xList, yList)
 	return reg.coef_
 
@@ -62,13 +68,33 @@ def main():
 	xList, yList = loadDataSet("/home/liud/PycharmProjects/Machine_Learning/Regression/data/abalone.txt")
 	xArr, yArr = regularize(xList, yList) #标准化
 	yArr = np.transpose([yArr])
-	
+	nText = 30
+	_, n = np.shape(xArr)
+	wArr = np.zeros([nText, n])
+	while (1):
+		print '请输入你选择的方式(1.skLearn;2.regression自己实现的岭回归)'
+		selectStyle = raw_input()
+		if selectStyle == '1':
+			for i in xrange(nText):
+				# 根据skLearn调包实现的岭回归
+				ws = skLearn_LassoLeastAngleRegression(xArr, yArr, exp(i - 10))
+				wArr[i, :] = ws
+			break
+		elif selectStyle == '2':
+			for i in xrange(nText):
+				# 自己按理解实现
+				ws = lassoLeastAngleRegression(xArr, yArr, 0.004)
+				wArr[i, :] = ws
+			break
+		else:
+			print '错误输入,请重新输入'
+	print wArr
 
 	#绘制轨迹
 	fig = plt.figure()
 	ax = fig.add_subplot(111)
-	lam = [i - 10 for i in xrange(nTest)]
-	ax.plot(lam, ws)
+	lam = [i - 10 for i in xrange(nText)]
+	ax.plot(lam, wArr)
 	plt.show()
 
 if __name__ == '__main__':
